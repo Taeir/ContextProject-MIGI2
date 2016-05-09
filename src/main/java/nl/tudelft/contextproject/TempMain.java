d596d4a3ef3d8adb2ee83906c944d659cd236fd8
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-09 09:24:26 +0200

    Created temporary class for jMonkey research
    Used to follow some tutorials on jMonkey

---
+++ src/main/java/nl/tudelft/contextproject/TempMain.java
@@ -0,0 +1,26 @@
+package nl.tudelft.contextproject;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * Temporary class for examining FP control schemes and collisions.
+ *
+ */
+public class TempMain extends SimpleApplication {
+
+	/**
+	 * Call instance to start.
+	 * @param args arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
