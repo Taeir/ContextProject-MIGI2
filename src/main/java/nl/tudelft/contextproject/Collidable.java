97f3f86d212dd7fe0f50efe95d0bcc3535324f03
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-10 12:53:32 +0200

    Added collidable interface
    Base interface structure.

---
+++ src/main/java/nl/tudelft/contextproject/Collidable.java
@@ -0,0 +1,9 @@
+package nl.tudelft.contextproject;
+
+/**
+ * Collidable interface.
+ *	Each object that uses some kind of physics should implement this class.
+ */
+public interface Collidable {
+
+}
