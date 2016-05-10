36e7e8e2f78055e0514907c6c760463e1faa3903
Chiel <chielbruin@tele2.nl>
2016-05-10 16:07:14 +0200

    Added getter for game instance.
    Added a getter for retrieving the current game instance.

--- src/main/java/nl/tudelft/contextproject/Main.java
+++ src/main/java/nl/tudelft/contextproject/Main.java
@@ -12,6 +12,7 @@ import com.jme3.scene.Node;
 import nl.tudelft.contextproject.controller.Controller;
 import nl.tudelft.contextproject.controller.GameController;
 import nl.tudelft.contextproject.controller.GameState;
+import nl.tudelft.contextproject.model.Game;
 import nl.tudelft.contextproject.model.TickListener;
 import nl.tudelft.contextproject.model.level.RandomLevelFactory;
 
@@ -51,6 +52,18 @@ public class Main extends SimpleApplication {
 			return true;
 		}
 		return false;
+	}
+	
+	/**
+	 * Get the instance of the current game.
+	 * @return the current instance of the game.
+	 * @throws IllegalStateException when the current controller is not a game Controller.
+	 */
+	public Game getCurrentGame() throws IllegalStateException {
+		if (!getGameState().isStarted() || !(controller instanceof GameController)) {
+			throw new IllegalStateException("The game is not running!");
+		}
+		return ((GameController) controller).getGame();		
 	}
 	
 	/**
