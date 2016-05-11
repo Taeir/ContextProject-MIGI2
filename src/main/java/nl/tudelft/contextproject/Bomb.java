705b45d7e2177aa9e0c72d4eec91ec44c2fd2a50
Wytze Elhorst <welhorst@hotmail.nl>
2016-05-11 10:19:50 +0200

    new branch for no merge conflicts

---
+++ src/main/java/nl/tudelft/contextproject/Bomb.java
@@ -0,0 +1,63 @@
+package nl.tudelft.contextproject;
+
+import java.awt.Graphics2D;
+
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Sphere;
+
+/**
+ * Class representing the player wearing the VR headset.
+ */
+public class Bomb extends Entity {
+	private Geometry geometry;
+	private Spatial sp;
+	/**
+	 * Constructor for a default player.
+	 * This player is (for now) a red sphere.
+	 */
+	public Bomb() {
+		Node nod = (Node) Main.getInstance().getAssetManager().loadModel("Models/Bomb.j3o");
+		Material mat = new Material(Main.getInstance().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Red);
+		nod.setMaterial(mat);
+		Material matb = new Material(Main.getInstance().getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+		matb.setColor("Color", ColorRGBA.White);
+		((Node) nod.getChild("Cylinder.001")).getChild(0).setMaterial(matb);
+		sp = nod;
+	}
+
+	@Override
+	public Geometry getGeometry() {
+		return geometry;
+	}
+	public Spatial getSpatial(){
+		return sp;
+	}
+
+	@Override
+	public void simpleUpdate(float tdf) {
+		sp.move(0, 0, 0);
+	}
+
+	@Override
+	public void mapDraw(Graphics2D g, int resolution) {
+		Vector3f trans = geometry.getLocalTranslation();
+		int x = (int) trans.x * resolution;
+		int y = (int) trans.y * resolution;
+		int width = resolution / 2;
+		int offset = resolution / 4;
+
+		g.fillOval(x + offset, y + offset, width, width);
+	}
+
+	@Override
+	public void setGeometry(Geometry geo) {
+		geometry = geo;
+		
+	}
+}
---
+++ src/test/java/nl/tudelft/contextproject/BombTest.java
@@ -0,0 +1,49 @@
+package nl.tudelft.contextproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Abstract test class for bomb.
+ */
+public abstract class BombTest extends EntityTest{
+	
+	private Bomb bomb;
+	
+	public Drawable getDrawable() {
+		return getBomb();
+	}
+	
+	/**
+	 * Getter for a specific instance of bomb.
+	 * @return an bomb to test with.
+	 */
+	public abstract Bomb getBomb();
+
+	/**
+	 * create a new (clean) bomb to test with.
+	 */
+	public void setupbomb() {
+		bomb = getBomb();
+	}
+	
+	/**
+	 * Test if the initial state of an bomb is NEW.
+	 */
+	@Test
+	public void testInitialState() {
+		setupbomb();
+		assertEquals(bomb.getState(), EntityState.NEW);
+	}
+	
+	/**
+	 * Test if setting a new state changes the state.
+	 */
+	@Test
+	public void testSetState() {
+		setupbomb();
+		bomb.setState(EntityState.ALIVE);
+		assertEquals(bomb.getState(), EntityState.ALIVE);
+	}
+}
