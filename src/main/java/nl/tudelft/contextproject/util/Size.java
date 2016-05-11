2b3d188f5203e56f273130ae2fabf629e4b52a91
Bram Crielaard <bram@crielaard.co.uk>
2016-05-11 09:26:15 +0200

    Shorten Size
    This class now goes wild with lombok.

--- src/main/java/nl/tudelft/contextproject/util/Size.java
+++ src/main/java/nl/tudelft/contextproject/util/Size.java
@@ -2,24 +2,16 @@ package nl.tudelft.contextproject.util;
 
 import lombok.Getter;
 import lombok.ToString;
+import lombok.AllArgsConstructor;
 
 /**
  * Class for storing a pair of width and height values.
- * This class makes use of the lombok ToString and getter tags, which means
+ * This class makes use of the lombok ToString, getter and constructor tags, which means
  * these methods are available but aren't described in the code.
  */
 @ToString
+@AllArgsConstructor
 public class Size {
     @Getter private final int width;
     @Getter private final int height;
-
-    /**
-     * Create a "Size" pair.
-     * @param width - The width contained in the pair.
-     * @param height - The height contained in the pair.
-     */
-    public Size(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
 }
