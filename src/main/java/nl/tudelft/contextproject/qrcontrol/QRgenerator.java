956f9412aa768198a1af9387f7d4982c7af01199
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-17 11:11:38 +0200

    Added QRgenerator class

---
+++ src/main/java/nl/tudelft/contextproject/qrcontrol/QRgenerator.java
@@ -0,0 +1,19 @@
+package nl.tudelft.contextproject.qrcontrol;
+
+/**
+ *	QR code generator class. 
+ *	Singleton that will generate a QR code and place it in "src/main/assets/QRcode/qrcode.png".
+ *
+ */
+public final class QRgenerator {
+	
+	
+	
+	/**
+	 * Private constructor to prevent initialization.
+	 */
+	private QRgenerator() {
+		//To prevent initialization.
+	}
+
+}
