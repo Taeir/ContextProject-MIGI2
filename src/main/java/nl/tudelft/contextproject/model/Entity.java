cda4716993900e4ddfefc9694e905e9a5688a2eb
Chiel <chielbruin@tele2.nl>
2016-05-17 11:20:40 +0200

    Added method to entity for collision with player
    Added a method that can be called to detect collisions between entities
    and the player.

--- src/main/java/nl/tudelft/contextproject/model/Entity.java
+++ src/main/java/nl/tudelft/contextproject/model/Entity.java
@@ -1,5 +1,9 @@
 package nl.tudelft.contextproject.model;
 
+import com.jme3.math.Vector3f;
+
+import nl.tudelft.contextproject.Main;
+
 /**
  * Abstract parent class for all entities.
  * All entities have a state, are Drawable and are Collidable.
@@ -21,5 +25,16 @@ public abstract class Entity implements Drawable, TickListener {
 	 */
 	public void setState(EntityState newState) {
 		this.state = newState;
+	}
+	
+	/**
+	 * Checks if this object is closer to the player than the specified distance.
+	 * @param dist The maximum distance to the player to detect the collision.
+	 * @return True if the player is closert than dist.
+	 */
+	public boolean collidesWithPlayer(float dist) {
+		Vector3f playerLoc = Main.getInstance().getCurrentGame().getPlayer().getSpatial().getLocalTranslation();
+		Vector3f thisLoc = getSpatial().getLocalTranslation();
+		return thisLoc.distance(playerLoc) < dist;
 	}
 }
--- src/test/java/nl/tudelft/contextproject/model/EntityTest.java
+++ src/test/java/nl/tudelft/contextproject/model/EntityTest.java
@@ -1,8 +1,16 @@
 package nl.tudelft.contextproject.model;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+import nl.tudelft.contextproject.Main;
+import nl.tudelft.contextproject.test.TestUtil;
 
 /**
  * Abstract test class for Entity.
@@ -46,5 +54,43 @@ public abstract class EntityTest extends DrawableTest {
 		setupEntity();
 		entity.setState(EntityState.ALIVE);
 		assertEquals(entity.getState(), EntityState.ALIVE);
+	}
+	
+	/**
+	 * Test if collision detection returns true when it should.
+	 */
+	@Test
+	public void testPlayerCollisionTrue() {
+		TestUtil.mockGame();
+		setupEntity();
+		
+		Spatial eSpat = mock(Spatial.class);
+		Spatial pSpat = mock(Spatial.class);
+		entity.setSpatial(eSpat);		
+		Main.getInstance().getCurrentGame().getPlayer().setSpatial(pSpat);
+		
+		when(eSpat.getLocalTranslation()).thenReturn(new Vector3f(0, 0, 0));
+		when(pSpat.getLocalTranslation()).thenReturn(new Vector3f(0, 0, .199f));
+		
+		assertTrue(entity.collidesWithPlayer(.2f));
+	}
+	
+	/**
+	 * Test if collision detection returns flase when it should.
+	 */
+	@Test
+	public void testPlayerCollisionFalse() {
+		TestUtil.mockGame();
+		setupEntity();
+		
+		Spatial eSpat = mock(Spatial.class);
+		Spatial pSpat = mock(Spatial.class);
+		entity.setSpatial(eSpat);		
+		Main.getInstance().getCurrentGame().getPlayer().setSpatial(pSpat);
+		
+		when(eSpat.getLocalTranslation()).thenReturn(new Vector3f(0, 0, 0));
+		when(pSpat.getLocalTranslation()).thenReturn(new Vector3f(0, 0, .201f));
+		
+		assertFalse(entity.collidesWithPlayer(.2f));
 	}
 }
