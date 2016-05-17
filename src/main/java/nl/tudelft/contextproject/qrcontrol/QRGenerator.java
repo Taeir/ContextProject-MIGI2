b945ccc6fc01dba7b0de927a3183877db8e70cc5
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-17 13:13:30 +0200

    QRcode generator implemented and gitignore updated

--- src/main/java/nl/tudelft/contextproject/qrcontrol/QRGenerator.java
+++ src/main/java/nl/tudelft/contextproject/qrcontrol/QRGenerator.java
@@ -1,21 +1,34 @@
 package nl.tudelft.contextproject.qrcontrol;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
 import nl.tudelft.contextproject.logging.Log;
 
 /**
  *	QR code generator class. 
- *	Singleton that will generate a QR code and place it in "src/main/assets/QRcode/qrcode.png".
+ *	Singleton that will generate a QR code and place it in {@link #LOCATION}.
  */
 public final class QRGenerator {
 
 	//Use eager initialization of the singleton.
 	private static final QRGenerator INSTANCE = new QRGenerator();
 
+	//Location and name of QR image.
+	private static final String LOCATION = "qrcode.png";
+	//Width of QR image.
+	private static final int WIDTH = 250;
+	//Height of QR image.
+	private static final int HEIGTH = 250;
+	
 	/**
 	 * Private constructor to prevent initialization elsewhere.
 	 */
@@ -31,9 +44,10 @@ public final class QRGenerator {
 	}
 
 	/**
-	 * Generate a QR code in "src/main/assets/QRcode/qrcode.png".
+	 * Generate a QR code in {@link #LOCATION}.
 	 * First, get the hostingAddress by using the Java InetAddress class.
-	 * Then, create the QRgen class.
+	 * Then, create the QRgen as a ByteArrayOutputStream.
+	 * And finally write the ByteArrayOutputStream to disk.
 	 * 
 	 * @param portNumber
 	 * 				the port number which hosts the application
@@ -41,30 +55,40 @@ public final class QRGenerator {
 	public static void generateQRcode(String portNumber) {
 		String hostingAddress = INSTANCE.getIP();
 		hostingAddress = "https://" + hostingAddress + ":" + portNumber + "/";
+		
 		Log.getLog("WebInterface").info("Creating QRcode with address: " + hostingAddress);
+		ByteArrayOutputStream byteArrayOutputStream = QRCode.from(hostingAddress).to(ImageType.PNG).withSize(WIDTH, HEIGTH).stream();
+		
+		try (OutputStream outputStream = new FileOutputStream(LOCATION)) {
+		    byteArrayOutputStream.writeTo(outputStream);
+		    Log.getLog("WebInterface").info("Created QRcode with address: " + hostingAddress + " as " + LOCATION);
+		} catch (IOException e) {
+			Log.getLog("WebInterface").severe("Unable to write qr code to disk.", e);
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Get the correct ipv4 address of this computer.
-	 * This method simply get all network interfaces of the computer.
+	 * This method gets all network interfaces of the computer.
 	 * Then for each network interface check all the network addresses.
 	 * The IP address will be among those, so all address that are local or LAN based
 	 * are filtered out.
-	 * Please note that you cannot have other network adapters from virtual machines
+	 * Please note that you cannot have other network adapters (such from virtual machines)
 	 * running as they will interfere with the correct adapter.
 	 * @return
 	 * 			IP address of this computer.
 	 */
 	protected String getIP() {
 		String  ip = "";
-		Enumeration<NetworkInterface> n;
+		
 		try {
-			n = NetworkInterface.getNetworkInterfaces();
-			
+			Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+
 			while (n.hasMoreElements()) {
 				NetworkInterface e = n.nextElement();
 				Enumeration<InetAddress> a = e.getInetAddresses();
-				
+
 				while (a.hasMoreElements()) {
 					InetAddress addr = a.nextElement();
 					String hostAddress = addr.getHostAddress();
@@ -74,9 +98,9 @@ public final class QRGenerator {
 					}
 				}
 			}
-		} catch (SocketException e1) {
-			Log.getLog("WebInterface").severe("No socket", e1);
-			e1.printStackTrace();
+		} catch (SocketException e) {
+			Log.getLog("WebInterface").severe("No socket", e);
+			e.printStackTrace();
 		}
 
 		return ip;
