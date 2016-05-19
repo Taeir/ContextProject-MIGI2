f39a05fc468319c6efb2c40e0ab12cc9c10346c6
Chiel <chielbruin@tele2.nl>
2016-05-19 13:09:47 +0200

    Removed temporary method

--- src/main/java/nl/tudelft/contextproject/controller/WaitingController.java
+++ src/main/java/nl/tudelft/contextproject/controller/WaitingController.java
@@ -1,13 +1,7 @@
 package nl.tudelft.contextproject.controller;
 
 import java.io.FileNotFoundException;
-import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
-import com.jme3.app.state.AppStateManager;
-import com.jme3.input.controls.ActionListener;
-
-import nl.tudelft.contextproject.Main;
-import nl.tudelft.contextproject.model.level.RandomLevelFactory;
 
 /**
  * A controller for the waiting state of the game.
@@ -24,21 +18,6 @@ public class WaitingController extends GameController {
 	 */
 	public WaitingController(SimpleApplication app) throws FileNotFoundException {
 		super(app, "/maps/" + MENU_LEVEL + "/");
-	}
-	
-	/**
-	 * Temporary method to enable the player to start the main game by pressing the pause button.
-	 */
-	@Override
-	public void initialize(AppStateManager stateManager, Application app) {
-		super.initialize(stateManager, app);
-		Main main = Main.getInstance();
-		main.getInputManager().addListener(new ActionListener() {			
-			@Override
-			public void onAction(String name, boolean isPressed, float tpf) {
-				main.setController(new GameController(main, (new RandomLevelFactory(5, false)).generateRandom()));
-			}
-		}, "pause");
 	}
 
 	@Override
