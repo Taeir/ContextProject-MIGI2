33f25d3a6219c2bdbe358da541abfee58408fc31
Chiel <chielbruin@tele2.nl>
2016-05-20 10:44:03 +0200

    Added controller support
    Added support for the xBox 360 controller.
    Walking is still a bit picky, but it works for now.

--- src/main/java/nl/tudelft/contextproject/Main.java
+++ src/main/java/nl/tudelft/contextproject/Main.java
@@ -7,12 +7,16 @@ import java.util.List;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
+import com.jme3.input.CameraInput;
 import com.jme3.input.InputManager;
+import com.jme3.input.Joystick;
 import com.jme3.input.KeyInput;
+import com.jme3.input.controls.JoyAxisTrigger;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
+import com.jme3.system.AppSettings;
 
 import nl.tudelft.contextproject.audio.AudioManager;
 import nl.tudelft.contextproject.audio.BackgroundMusic;
@@ -51,6 +55,10 @@ public class Main extends SimpleApplication {
 		Main main = getInstance();
 		List<String> a = Arrays.asList(args);
 		debugHud = a.contains("--debugHud");
+		
+		AppSettings settings = new AppSettings(true);
+        settings.setUseJoysticks(true);
+        main.setSettings(settings);
 		main.start();
 	}
 
@@ -138,7 +146,8 @@ public class Main extends SimpleApplication {
 		setDisplayStatView(debugHud);
 		
 		//TODO if VR support is implemented the flyby camera should be disabled
-		getFlyByCamera().setMoveSpeed(100);
+		getFlyByCamera().setZoomSpeed(0);
+		
 		getViewPort().setBackgroundColor(new ColorRGBA(0.1f, 0.1f, 0.1f, 1f));
 		getCamera().lookAtDirection(new Vector3f(0, 1, 0), new Vector3f(0, 1, 0));
 		
@@ -167,12 +176,26 @@ public class Main extends SimpleApplication {
 	 * Setup all the key mappings.
 	 */
 	protected void setupControlMappings() {
-		getInputManager().addMapping("pause", new KeyTrigger(KeyInput.KEY_P));
-		getInputManager().addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-		getInputManager().addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-		getInputManager().addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-		getInputManager().addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-		getInputManager().addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+		InputManager im = getInputManager();		
+		if (isControllerConnected()) {
+			getFlyByCamera().onAction(CameraInput.FLYCAM_INVERTY, false, 0);
+			Joystick j = im.getJoysticks()[0];
+		
+			im.addMapping("Up", new JoyAxisTrigger(0, 0, true));
+			im.addMapping("Down", new JoyAxisTrigger(0, 0, false));
+			im.addMapping("Left", new JoyAxisTrigger(0, 1, true));
+			im.addMapping("Right", new JoyAxisTrigger(0, 1, false));
+			
+			j.getButton("0").assignButton("Jump");				// A
+			j.getButton("3").assignButton("SIMPLEAPP_Exit");	// Y
+		} else {
+			im.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+			im.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+			im.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+			im.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+			im.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+		}
+		im.addMapping("pause", new KeyTrigger(KeyInput.KEY_P));
 	}
 	
 	//TODO this will be removed when camera type is changed
@@ -267,6 +290,15 @@ public class Main extends SimpleApplication {
 	 */
 	public static boolean isDebugHudShown() {
 		return debugHud;
+	}
+
+	/**
+	 * Check if a controller is connected.
+	 * @return True if a controller is connected, false otherwise.
+	 */
+	public boolean isControllerConnected() {
+		Joystick[] sticks = getInputManager().getJoysticks();
+		return sticks != null && sticks.length > 0;
 	}
 	
 	
