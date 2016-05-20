1f358d19309e04494161c075ecf6c9f9410efd50
Bram Crielaard <bram@crielaard.co.uk>
2016-05-20 18:01:23 +0200

    Fix controller comments
    Fixes all comments in the controller package.

--- src/main/java/nl/tudelft/contextproject/controller/GameState.java
+++ src/main/java/nl/tudelft/contextproject/controller/GameState.java
@@ -4,29 +4,16 @@ package nl.tudelft.contextproject.controller;
  * Enum representing the state of the game.
  */
 public enum GameState {
-	/**
-	 * Waiting for the game to start.
-	 */
 	WAITING,
-	
-	/**
-	 * The main game is running.
-	 */
 	RUNNING,
-	
-	/**
-	 * The main game is suspended.
-	 */
 	PAUSED,
-	
-	/**
-	 * The main game is finished.
-	 */
 	ENDED;
 	
 	/**
 	 * Check if the state represents a started game.
-	 * @return True if the state represents a started game, false otherwise.
+	 *
+	 * @return
+	 * 		true if the state represents a started game, false otherwise
 	 */
 	public boolean isStarted() {
 		return this == RUNNING || this == PAUSED;
