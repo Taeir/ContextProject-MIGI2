7ac73bf456cb50a59153830413a4e60b3d2c32f4
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-24 11:05:26 +0200

    Added constructor and filename to Room class

--- src/main/java/nl/tudelft/contextproject/model/level/Room.java
+++ src/main/java/nl/tudelft/contextproject/model/level/Room.java
@@ -1,6 +1,9 @@
 package nl.tudelft.contextproject.model.level;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.jme3.light.Light;
 
@@ -15,19 +18,50 @@ import nl.tudelft.contextproject.util.Size;
  */
 public class Room {
 
+	private static final Pattern PATTERN = Pattern.compile("(?<width>\\d+)x(?<height>\\d+)_.*");
+
 	//Room size 
 	public Size size;
-	
+
 	//List of entities in the room
 	public List<Entity> entities;
-	
+
 	//List of lights in the room
 	public List<Light> lights;
-	
-	//List of tiles in the room
-	public List<MazeTile> tiles;
-	
-	
-	
-	
+
+	//2d representation of tiles in the room
+	public TileType[][] tiles;
+
+	/**
+	 * Constructor will load room from files using RoomIO.
+	 * @param fileName
+	 * 		fileName of the room
+	 */
+	public Room(String fileName) {
+		entities = new ArrayList<Entity>();
+		lights = new ArrayList<Light>();
+		setSizeFromFileName(fileName);
+
+
+	}
+
+	/**
+	 * Get size of room from file name.
+	 * @param fileName
+	 * 		fileName of the Room
+	 */
+	protected void setSizeFromFileName(String fileName) {
+		int width = 0;
+		int height = 0;
+		Matcher m = PATTERN.matcher(fileName);
+		if (m.matches()) {
+			width = Integer.parseInt(m.group("width"));
+			height = Integer.parseInt(m.group("height"));
+			size = new Size(width, height);
+		} 
+	}
+
+
+
+
 }
