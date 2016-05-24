8e7610ccf69a05bb595b707a68f74ac4ad968d15
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-24 12:12:19 +0200

    Tested equals method of size class

--- src/test/java/nl/tudelft/contextproject/model/level/RoomTest.java
+++ src/test/java/nl/tudelft/contextproject/model/level/RoomTest.java
@@ -2,13 +2,30 @@ package nl.tudelft.contextproject.model.level;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test class for Room.
+ */
 public class RoomTest {
 
+	private static final String ROOM_FOLDER = "/maps/correctWithName/";
+	
+	private Room testRoom;
+	
+	
+	@Before
+	public void setUpSimpleRoom() {
+		testRoom = new Room(ROOM_FOLDER);
+	}
+	
+	/**
+	 * Test constructor on test room.
+	 */
 	@Test
-	public void testRoom() {
-		fail("Not yet implemented");
+	public void testRoomConstructorSize() {
+		
 	}
 
 	@Test
