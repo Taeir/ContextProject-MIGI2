9bedd182f45e2fecbbfa3dfab99765f41a26b39b
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-25 21:31:28 +0200

    Added a few test for the findMapFile method

--- src/test/java/nl/tudelft/contextproject/model/level/roomIO/MapReaderTest.java
+++ src/test/java/nl/tudelft/contextproject/model/level/roomIO/MapReaderTest.java
@@ -1,22 +1,53 @@
 package nl.tudelft.contextproject.model.level.roomIO;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import org.junit.Test;
+
+import nl.tudelft.contextproject.util.FileUtil;
 
 /**
  * Test class for MapReader.
  */
 public class MapReaderTest {
 
+	//Location of the test map
+	private static final String TEST_MAP_LOCATION = "/maps/testGridMap/";
+	//Location of the test file
+	private static final String TEST_MAP_FILE_NAME = "/maps/testGridMap/mapsettings.cmf";
+
 	@Test
 	public void testReadMap() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Try to get gridTestMap.
+	 */
 	@Test
 	public void testGetMapFile() {
-		fail("Not yet implemented");
+		try {
+			File testFile = MapReader.getMapFile(TEST_MAP_LOCATION);
+			assertTrue(testFile.exists());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test exception if .cmf file does not exist.
+	 * @throws FileNotFoundException 
+	 * 			this should happen here
+	 */
+	@Test (expected = FileNotFoundException.class)
+	public void testGetMapFileException() throws FileNotFoundException {
+		File testFile = MapReader.getMapFile("/maps/correctWithName/");
+		assertTrue(testFile.exists());
 	}
 
 }
