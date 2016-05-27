4cef6ee185defcee7216cf9a9128452652eb4341
Robin van der Wal <apocalytica8@hotmail.com>
2016-05-27 11:29:59 +0200

    RandomUtil class and tests

--- src/main/java/nl/tudelft/contextproject/model/level/MSTBasedLevelFactory.java
+++ src/main/java/nl/tudelft/contextproject/model/level/MSTBasedLevelFactory.java
@@ -14,6 +14,15 @@ import nl.tudelft.contextproject.model.level.roomIO.MapReader;
  */
 public class MSTBasedLevelFactory implements LevelFactory {
 
+	//Max width of level 
+	protected static final int MAX_WIDTH = 200;
+	//Max height of level 
+	protected static final int MAX_HEIGHT = 200;
+	//Number of attempts 
+	private static final int MAX_ATTEMPTS = 1000;
+	//Minimum distance from Edges
+	private static final int MINIMUM_EDGE_DISTANCE = 10;
+	
 	private Random rand;
 	
 	private ArrayList<Room> rooms;
@@ -22,7 +31,7 @@ public class MSTBasedLevelFactory implements LevelFactory {
 	
 	private ArrayList<Light> lights;
 	
-	private MazeTile[][] mazeTile;
+	private TileType[][] tiles;
 	
 	
 	/**
@@ -33,24 +42,40 @@ public class MSTBasedLevelFactory implements LevelFactory {
 	public MSTBasedLevelFactory(String mapFolder) {
 		rooms = new ArrayList<Room>();
 		lights = new ArrayList<Light>();
+		tiles = new TileType[MAX_WIDTH][MAX_HEIGHT];
 		initializeBuilder(mapFolder);
 	}
 	
+	/**
+	 * Generate a level in 6 steps.
+	 *<ol>
+	 *	<li>Place start and treasure room.</li>
+	 *	<li>Place randomly rooms for some amount of attempts.</li>
+	 *	<li>Create an edge from every entrance door to every exit door of other rooms.</li>
+	 *	<li>Create MST. </li>
+	 *	<li>For every edge still existing use breadth first search to find corridors.</li>
+	 *	<li>Beautify corridors.</li>
+	 *</ol>
+	 */
 	@Override
 	public Level generateSeeded(long seed) {
 		createRNG(seed);
-		createLevelGraph();
+		placeStartAndTreasureRoom();
 
 		return new Level(null, null);
 	}
 	
 	/**
-	 * Ask the GraphLevel class to create a GraphLevel.
+	 * Place start and treasure room on semi-random locations.
+	 * Start room is placed in left-most quarter, treasure-room in right most quarter.
 	 */
-	protected void createLevelGraph() {
+	protected void placeStartAndTreasureRoom() {
+		int endLeftMostQuarter = (int) Math.round((double) MAX_WIDTH / 4.0);
+		int beginningRightMostQuarter = (int) Math.round(3.0 * (double) MAX_WIDTH / 4.0);
 		
 		
 	}
+
 
 	/**
 	 * Initialize all the data needed for building.
@@ -59,7 +84,7 @@ public class MSTBasedLevelFactory implements LevelFactory {
 	 */
 	protected void initializeBuilder(String mapFolder) {
 		try {
-			startAndEndRooms = MapReader.readMap(null, rooms);
+			startAndEndRooms = MapReader.readMap(mapFolder, rooms);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/nl/tudelft/contextproject/model/level/roomIO/RoomRotation.java
@@ -0,0 +1,28 @@
+package nl.tudelft.contextproject.model.level.roomIO;
+
+/**
+ * Enum for possible Rotations of rooms.
+ *
+ */
+public enum RoomRotation {
+
+	/**
+	 * Don't rotate at all.
+	 */
+	ROTATION_0,
+	
+	/**
+	 * Rotate 90 degrees.
+	 */
+	ROTATION_90,
+	
+	/**
+	 * Rotate 180 degrees, flip.
+	 */
+	ROTATION_180,
+	
+	/**
+	 * Rotate 240 degrees.
+	 */
+	ROTATION_240;
+}
