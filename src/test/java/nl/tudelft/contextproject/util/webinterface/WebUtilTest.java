c6c50fc8d48be2ddc998971082052530faad3741
Bram Crielaard <bram@crielaard.co.uk>
2016-05-27 22:55:33 +0200

    Add tests webutil
    Adds all needed tests to the WebUtil.

--- src/test/java/nl/tudelft/contextproject/util/webinterface/WebUtilTest.java
+++ src/test/java/nl/tudelft/contextproject/util/webinterface/WebUtilTest.java
@@ -1,16 +1,49 @@
 package nl.tudelft.contextproject.util.webinterface;
 
+import com.jme3.math.Vector3f;
+import nl.tudelft.contextproject.Main;
 import nl.tudelft.contextproject.TestBase;
+import nl.tudelft.contextproject.model.Game;
+import nl.tudelft.contextproject.model.entities.Entity;
+import nl.tudelft.contextproject.model.entities.VRPlayer;
+import nl.tudelft.contextproject.model.level.Level;
+import nl.tudelft.contextproject.model.level.MazeTile;
+import nl.tudelft.contextproject.model.level.TileType;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test for the WebUtil class.
  */
 public class WebUtilTest extends TestBase {
+	private Game mockedGame;
+	private Level mockedLevel;
+	private Vector3f zeroVector;
+	private VRPlayer mockedPlayer;
+
+	/**
+	 * Set up all needed fields for testing.
+	 */
+	@Before
+	public void setUp() {
+		mockedGame = mock(Game.class);
+		mockedLevel = mock(Level.class);
+		mockedPlayer = mock(VRPlayer.class);
+
+		zeroVector = new Vector3f(0.f, 0.f, 0.f);
+
+		when(Main.getInstance().getCurrentGame()).thenReturn(mockedGame);
+		when(mockedGame.getLevel()).thenReturn(mockedLevel);
+	}
 
 	/**
 	 * Test for decoding actions.
@@ -56,4 +89,76 @@ public class WebUtilTest extends TestBase {
 		assertFalse(WebUtil.checkValidDwarfs("wingame"));
 	}
 
+	/**
+	 * Test for checking if placing in the void is not a valid location.
+	 */
+	@Test
+	public void testCheckValidLocationNull() {
+		when(mockedLevel.getTile(0, 0)).thenReturn(null);
+		assertFalse(WebUtil.checkValidLocation(0, 0));
+	}
+
+	/**
+	 * Test for checking if placing in a wall is not a valid location.
+	 */
+	@Test
+	public void testCheckValidLocationWall() {
+		MazeTile tile = new MazeTile(0, 0, TileType.WALL);
+		when(mockedLevel.getTile(0, 0)).thenReturn(tile);
+		assertFalse(WebUtil.checkValidLocation(0, 0));
+	}
+
+	/**
+	 * Test for checking if placing in an entity is not a valid location.
+	 */
+	@Test
+	public void testCheckValidLocationEntity() {
+		MazeTile tile = new MazeTile(0, 0, TileType.FLOOR);
+		when(mockedLevel.getTile(0, 0)).thenReturn(tile);
+
+		Entity mockedEntity = mock(Entity.class);
+		when(mockedEntity.getLocation()).thenReturn(zeroVector);
+
+		Set<Entity> entities = new HashSet<>();
+		entities.add(mockedEntity);
+
+		when(mockedGame.getEntities()).thenReturn(entities);
+
+		assertFalse(WebUtil.checkValidLocation(0, 0));
+	}
+
+	/**
+	 * Test for checking if placing in the player is not a valid location.
+	 */
+	@Test
+	public void testCheckValidLocationPlayer() {
+		MazeTile tile = new MazeTile(0, 0, TileType.FLOOR);
+		when(mockedLevel.getTile(0, 0)).thenReturn(tile);
+		Set<Entity> entities = new HashSet<>();
+		when(mockedGame.getEntities()).thenReturn(entities);
+
+		when(mockedGame.getPlayer()).thenReturn(mockedPlayer);
+		when(mockedPlayer.getLocation()).thenReturn(zeroVector);
+
+		assertFalse(WebUtil.checkValidLocation(0, 0));
+	}
+
+	/**
+	 * Check if placing on a valid location is indeed a valid location.
+	 */
+	@Test
+	public void testCheckValidLocationValid() {
+		MazeTile tile = new MazeTile(0, 0, TileType.FLOOR);
+		when(mockedLevel.getTile(0, 0)).thenReturn(tile);
+		Set<Entity> entities = new HashSet<>();
+		when(mockedGame.getEntities()).thenReturn(entities);
+
+
+		when(mockedGame.getPlayer()).thenReturn(mockedPlayer);
+		Vector3f oneVector = new Vector3f(1, 1, 1);
+		when(mockedPlayer.getLocation()).thenReturn(oneVector);
+
+		assertTrue(WebUtil.checkValidLocation(0, 0));
+
+	}
 }
