f89cc524692420af2d1dca0d9560a8e0da2c7915
Taico Aerts <info@taico.nl>
2016-06-01 14:48:54 +0200

    Let EntityUtil use EntityType.getWebId()

--- src/main/java/nl/tudelft/contextproject/util/webinterface/EntityUtil.java
+++ src/main/java/nl/tudelft/contextproject/util/webinterface/EntityUtil.java
@@ -11,57 +11,11 @@ import nl.tudelft.contextproject.model.entities.Entity;
  * Class used to make entities usable for the web interface.
  */
 public final class EntityUtil {
-	private static final int DEFAULT = 0;
-	private static final int BOMB = 1;
-	private static final int DOOR = 2;
-	private static final int KEY = 3;
-	private static final int VRPLAYER = 4;
-	private static final int PLAYERTRIGGER = 5;
-	private static final int PITFALL = 6;
-	private static final int LANDMINE = 7;
-	private static final int CARROT = 8;
-	private static final int KILLERBUNNY = 9;
-	private static final int VOIDPLATFORM = 10;
 
 	/**
 	 * Private constructor to avoid initialization.
 	 */
-	private EntityUtil() {}
-
-	/**
-	 * Get the ID for a certain class.
-	 *
-	 * @param entity
-	 * 		the classname of the entity
-	 * @return
-	 * 		an int representing the entity
-	 */
-	public static int getJSONCoded(String entity) {
-		switch (entity) {
-			case "Bomb":
-				return BOMB;
-			case "Door":
-				return DOOR;
-			case "Key":
-				return KEY;
-			case "VRPlayer":
-				return VRPLAYER;
-			case "PlayerTrigger":
-				return PLAYERTRIGGER;
-			case "Pitfall":
-				return PITFALL;
-			case "LandMine":
-				return LANDMINE;
-			case "Carrot":
-				return CARROT;
-			case "KillerBunny":
-				return KILLERBUNNY;
-			case "VoidPlatform":
-				return VOIDPLATFORM;
-			default:
-				return DEFAULT;
-		}
-	}
+	private EntityUtil() { }
 	
 	/**
 	 * Convert a set of entities to a JSONArray representing this list.
@@ -99,7 +53,7 @@ public final class EntityUtil {
 		JSONObject entity = new JSONObject();
 		entity.put("x", Math.round(e.getLocation().getX()));
 		entity.put("y", Math.round(e.getLocation().getZ()));
-		entity.put("type", EntityUtil.getJSONCoded(e.getClass().getSimpleName()));
+		entity.put("type", e.getType().getWebId());
 		return entity;
 	}
 }
