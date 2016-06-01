aa423c7532524924bcaa7e4924ea383d8d368f70
Taico Aerts <info@taico.nl>
2016-06-01 14:21:37 +0200

    Removed unneeded check

--- src/main/java/nl/tudelft/contextproject/model/entities/VRPlayer.java
+++ src/main/java/nl/tudelft/contextproject/model/entities/VRPlayer.java
@@ -205,16 +205,16 @@ public class VRPlayer extends MovingEntity implements PhysicsObject {
 	 * Player drops a bomb from his inventory.
 	 */
 	public void dropBomb() {
-		if (inventory.containsBomb()) {
-			Bomb bomb = new Bomb();
-			inventory.remove(bomb);
-			Vector3f vec = this.getSpatial().getLocalTranslation();
-			bomb.move((int) vec.x, (int) vec.y + 1, (int) vec.z);
-			bomb.activate();
-			if (Main.getInstance().getCurrentGame() != null) {
-				Main.getInstance().getCurrentGame().addEntity(bomb);
-			}
-		}
+		if (!inventory.containsBomb()) return;
+		
+		Bomb bomb = new Bomb();
+		inventory.remove(bomb);
+		
+		Vector3f vec = this.getSpatial().getLocalTranslation();
+		bomb.move((int) vec.x, (int) vec.y + 1, (int) vec.z);
+		bomb.activate();
+		
+		Main.getInstance().getCurrentGame().addEntity(bomb);
 	}
 	
 	/**
