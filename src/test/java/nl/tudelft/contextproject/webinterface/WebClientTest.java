faff50f86a08301b8285ea36a21fb8f0d72add6c
Taico Aerts <info@taico.nl>
2016-06-06 16:47:26 +0200

    Fixed WebClient tests and added more tests
    WebClient now has 100% line coverage (EclipseEmma)

--- src/test/java/nl/tudelft/contextproject/webinterface/WebClientTest.java
+++ src/test/java/nl/tudelft/contextproject/webinterface/WebClientTest.java
@@ -1,15 +1,23 @@
 package nl.tudelft.contextproject.webinterface;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.websocket.api.RemoteEndpoint;
+import org.json.JSONObject;
 import org.junit.Test;
 
-import nl.tudelft.contextproject.TestBase;
+import nl.tudelft.contextproject.webinterface.websockets.COCSocket;
 
 /**
  * Test class for {@link WebClient}.
  */
-public class WebClientTest extends TestBase {
+public class WebClientTest extends WebTestBase {
 
 	/**
 	 * Tests {@link WebClient#isElf}.
@@ -65,6 +73,160 @@ public class WebClientTest extends TestBase {
 		assertFalse(client.isElf());
 		assertEquals(Team.NONE, client.getTeam());
 	}
+	
+	/**
+	 * Tests {@link WebClient#sendMessage(String, HttpServletResponse)}, when sending a message
+	 * normally.
+	 * 
+	 * @throws IOException
+	 * 		will not happen because of mocks
+	 */
+	@Test
+	public void testSendMessage_normal() throws IOException {
+		WebClient client = new WebClient();
+		HttpServletResponse response = createMockedResponse();
+		
+		client.sendMessage("MESSAGE", response);
+		
+		verify(response).setStatus(HttpStatus.OK_200);
+		verify(response.getWriter()).write("MESSAGE");
+	}
+	
+	/**
+	 * Tests {@link WebClient#sendMessage(String, HttpServletResponse)}, when sending a message
+	 * via a websocket.
+	 * 
+	 * @throws IOException
+	 * 		will not happen because of mocks
+	 */
+	@Test
+	public void testSendMessage_socket() throws IOException {
+		COCSocket socket = mock(COCSocket.class);
+		when(socket.getRemote()).thenReturn(mock(RemoteEndpoint.class));
+		
+		WebClient client = new WebClient();
+		client.setWebSocket(socket);
+		
+		client.sendMessage("A", null);
+		
+		verify(socket.getRemote()).sendStringByFuture("A");
+	}
+	
+	/**
+	 * Tests {@link WebClient#sendMessage(JSONObject, HttpServletResponse)}, when sending a JSON
+	 * message normally.
+	 * 
+	 * @throws IOException
+	 * 		will not happen because of mocks
+	 */
+	@Test
+	public void testSendMessage_json_normal() throws IOException {
+		WebClient client = new WebClient();
+		HttpServletResponse response = createMockedResponse();
+		
+		JSONObject json = new JSONObject();
+		
+		client.sendMessage(json, response);
+		
+		verify(response).setStatus(HttpStatus.OK_200);
+		verify(response).setContentType("text/json");
+		verify(response.getWriter()).write(json.toString());
+	}
+	
+	/**
+	 * Tests {@link WebClient#sendMessage(JSONObject, HttpServletResponse)}, when sending a JSON
+	 * message via a websocket.
+	 * 
+	 * @throws IOException
+	 * 		will not happen because of mocks
+	 */
+	@Test
+	public void testSendMessage_json_socket() throws IOException {
+		COCSocket socket = mock(COCSocket.class);
+		when(socket.getRemote()).thenReturn(mock(RemoteEndpoint.class));
+		
+		WebClient client = new WebClient();
+		client.setWebSocket(socket);
+		
+		JSONObject json = new JSONObject();
+		
+		client.sendMessage(json, null);
+		
+		verify(socket.getRemote()).sendStringByFuture(json.toString());
+	}
+	
+	/**
+	 * Tests {@link WebClient#confirmMessage}.
+	 */
+	@Test
+	public void testConfirmMessage() {
+		WebClient client = new WebClient();
+		HttpServletResponse response = createMockedResponse();
+		
+		client.confirmMessage(response);
+		
+		verify(response).setStatus(HttpStatus.NO_CONTENT_204);
+	}
+	
+	/**
+	 * Tests {@link WebClient#getWebSocket} and {@link WebClient#setWebSocket}.
+	 */
+	@Test
+	public void testGetSetWebSocket() {
+		COCSocket socket = mock(COCSocket.class);
+		
+		WebClient client = new WebClient();
+		client.setWebSocket(socket);
+		
+		assertSame(socket, client.getWebSocket());
+	}
+	
+	/**
+	 * Tests {@link WebClient#removeWebSocket}, when the socket being removed is the current socket.
+	 */
+	@Test
+	public void testRemoveWebSocket_same() {
+		COCSocket socket = mock(COCSocket.class);
+		
+		WebClient client = new WebClient();
+		client.setWebSocket(socket);
+		
+		//Remove same
+		client.removeWebSocket(socket);
+		
+		//Should have been removed
+		assertNull(client.getWebSocket());
+	}
+	
+	/**
+	 * Tests {@link WebClient#removeWebSocket}, when the socket being removed is not the current
+	 * socket.
+	 */
+	@Test
+	public void testRemoveWebSocket_different() {
+		COCSocket socket = mock(COCSocket.class);
+		
+		WebClient client = new WebClient();
+		client.setWebSocket(socket);
+		
+		//Remove different
+		client.removeWebSocket(mock(COCSocket.class));
+		
+		//Socket should not have been removed
+		assertSame(socket, client.getWebSocket());
+	}
+	
+	/**
+	 * Tests {@link WebClient#getPerformedActions()}.
+	 */
+	@Test
+	public void testGetPerformedActions() {
+		WebClient client = new WebClient();
+		client.setTeam(Team.ELVES);
+		
+		//Elves should have 4 actions
+		assertEquals(4, client.getPerformedActions().size());
+	}
 
 	/**
 	 * Tests {@link WebClient#toString}.
@@ -75,7 +237,7 @@ public class WebClientTest extends TestBase {
 		WebClient client = new WebClient();
 		client.setTeam(Team.ELVES);
 		
-		assertEquals("WebClient<team=ELVES>", client.toString());
+		assertEquals("WebClient<team=" + client.getTeam() + ">", client.toString());
 	}
 
 }
