4af11c54469c42c5b921f3e6def916075bbdbe7c
Taico Aerts <info@taico.nl>
2016-06-06 16:28:51 +0200

    Cleaned up ClientServlet

--- src/main/java/nl/tudelft/contextproject/webinterface/ClientServlet.java
+++ src/main/java/nl/tudelft/contextproject/webinterface/ClientServlet.java
@@ -6,14 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.servlet.DefaultServlet;
-import org.json.JSONObject;
 
-import nl.tudelft.contextproject.Main;
-import nl.tudelft.contextproject.controller.EndingController;
 import nl.tudelft.contextproject.logging.Log;
-import nl.tudelft.contextproject.util.webinterface.EntityUtil;
 
 /**
  * Servlet for handling client requests.
@@ -21,7 +16,6 @@ import nl.tudelft.contextproject.util.webinterface.EntityUtil;
 public class ClientServlet extends DefaultServlet {
 	private static final long serialVersionUID = -8897739798010474802L;
 	
-	private static final String CONTENT_TYPE_JSON = "text/json";
 	private static final Log LOG = Log.getLog("WebInterface");
 
 	private final transient WebServer server;
@@ -71,7 +65,7 @@ public class ClientServlet extends DefaultServlet {
 				server.getNormalHandler().onMapRequest(request, response);
 				break;
 			case "status":
-				statusUpdate(request, response);
+				server.getNormalHandler().onStatusUpdateRequest(request, response);
 				break;
 			case "requestaction":
 				server.getNormalHandler().onActionRequest(request, response);
@@ -81,81 +75,5 @@ public class ClientServlet extends DefaultServlet {
 				super.doPost(request, response);
 				break;
 		}
-	}
-	
-	/**
-	 * If the given client is not null, this method returns true.
-	 * 
-	 * <p>If the client is null, UNAUTHORIZED is sent in the response, and this method returns
-	 * false.
-	 * 
-	 * @param client
-	 * 		the client to check
-	 * @param response
-	 * 		the response to write to in case of not authorized
-	 * @param json
-	 * 		if true, uses json for the response
-	 * @return
-	 * 		true if the client is authorized
-	 * 
-	 * @throws IOException
-	 * 		if writing to the response causes an IOException
-	 */
-	public boolean checkAuthorized(WebClient client, HttpServletResponse response, boolean json) throws IOException {
-		if (client != null) return true;
-		
-		//Client is not known: unauthorized request
-		response.setStatus(HttpStatus.OK_200);
-		if (json) {
-			response.setContentType(CONTENT_TYPE_JSON);
-			response.getWriter().write("{auth: false}");
-		} else {
-			response.getWriter().write("UNAUTHORIZED");
-		}
-		return false;
-	}
-	
-	/**
-	 * Handles a statusUpdate request.
-	 * 
-	 * @param request
-	 * 		the HTTP request
-	 * @param response
-	 * 		the HTTP response object
-	 * @throws IOException
-	 * 		if sending the response to the client causes an IOException
-	 */
-	public void statusUpdate(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		WebClient client = server.getUser(request);
-
-		if (!checkAuthorized(client, response, true)) return;
-
-		response.setStatus(HttpStatus.OK_200);
-		response.setContentType(CONTENT_TYPE_JSON);
-		
-		JSONObject json = new JSONObject();
-		json.put("team", client.getTeam().ordinal());
-		json.put("state", Main.getInstance().getGameState().name());
-		
-		switch (Main.getInstance().getGameState()) {
-			case WAITING:
-				//Fall through to running
-			case RUNNING:
-				json.put("entities",
-						EntityUtil.entitiesToJson(Main.getInstance().getCurrentGame().getEntities(), Main.getInstance().getCurrentGame().getPlayer()));
-				json.put("explored", Main.getInstance().getCurrentGame().getLevel().toExploredWebJSON());
-				break;
-			case PAUSED:
-				//We don't send any other data when the game is paused
-				break;
-			case ENDED:
-				Boolean elvesWin = ((EndingController) Main.getInstance().getController()).didElvesWin();
-				json.put("winner", elvesWin);
-				break;
-			default:
-				break;
-		}
-
-		response.getWriter().write(json.toString());
 	}
 }
