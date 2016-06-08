d7b04bd6125a4d867dc117badcfc03321065a1e1
Diocruel <apocalytica8@hotmail.com>
2016-06-08 15:46:22 +0200

    Changed javadoc of DoorLocation

--- src/main/java/nl/tudelft/contextproject/model/level/util/DoorLocation.java
+++ src/main/java/nl/tudelft/contextproject/model/level/util/DoorLocation.java
@@ -3,8 +3,6 @@ package nl.tudelft.contextproject.model.level.util;
 
 /**
  * Classes that implement this abstract class are a door in a room.
- * 
- * Used for determining if a door connection point has been used.
  */
 public abstract class DoorLocation {
 
@@ -15,7 +13,7 @@ public abstract class DoorLocation {
 	 * Update the door location if door has been moved.
 	 * 
 	 * @param coordinates
-	 * 					move coordinates.
+	 * 		move coordinates.
 	 */
 	public void updateDoorLocation(Vec2I coordinates) {
 		location.add(coordinates);
