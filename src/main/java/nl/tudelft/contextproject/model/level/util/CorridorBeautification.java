c14d20f3ff5ac5ca86858f8659ac6934d09f4224
Diocruel <apocalytica8@hotmail.com>
2016-06-08 20:05:09 +0200

    Added corridor beatification class

---
+++ src/main/java/nl/tudelft/contextproject/model/level/util/CorridorBeautification.java
@@ -0,0 +1,13 @@
+package nl.tudelft.contextproject.model.level.util;
+
+/**
+ * Corridor beautifications utility class. 
+ * Class that holds some methods to enhance the single tile wide corridor creation.
+ */
+public final class CorridorBeautification {
+
+	/**
+	 * Private constructor to prevent initialization.
+	 */
+	private CorridorBeautification() {}
+}
