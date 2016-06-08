0cb0904d91824218b269de2ae16f6b70617c4fca
Diocruel <apocalytica8@hotmail.com>
2016-06-08 14:05:26 +0200

    Tested VRLookManager fully.

--- src/test/java/nl/tudelft/contextproject/input/VRLookManagerTest.java
+++ src/test/java/nl/tudelft/contextproject/input/VRLookManagerTest.java
@@ -1,6 +1,5 @@
 package nl.tudelft.contextproject.input;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.Before;
@@ -9,6 +8,7 @@ import org.junit.Test;
 import com.jme3.input.CameraInput;
 import com.jme3.input.InputManager;
 import com.jme3.input.Joystick;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 
 import nl.tudelft.contextproject.TestBase;
@@ -19,12 +19,16 @@ import nl.tudelft.contextproject.TestBase;
 public class VRLookManagerTest extends TestBase {
 
 	private VRLookManager vrManager;
+	private Camera mockedCam;
 	/**
 	 * Set up a VRLookMAnager.
 	 */
 	@Before
 	public void setupTest() {
-		Camera mockedCam = mock(Camera.class);
+		mockedCam = mock(Camera.class);
+		when(mockedCam.getUp()).thenReturn(new Vector3f());
+		when(mockedCam.getLeft()).thenReturn(new Vector3f());
+		when(mockedCam.getDirection()).thenReturn(new Vector3f());
 		vrManager = spy(new VRLookManager(mockedCam));
 	}
 	
@@ -43,14 +47,34 @@ public class VRLookManagerTest extends TestBase {
 		verify(vrManager).mapJoystick(null);
 	}
 
+	/**
+	 * Test rotate camera.
+	 */
 	@Test
 	public void testRotateCamera() {
-		fail("Not yet implemented");
+		vrManager.rotateCamera(0.0f);
+		verify(mockedCam).setAxes(any());
 	}
 
+	/**
+	 * Test OnAnalog with fly camera Left.
+	 */
 	@Test
-	public void testOnAnalog() {
-		fail("Not yet implemented");
+	public void testOnAnalogFlyCameLeft() {
+		String name = CameraInput.FLYCAM_LEFT;
+		Float value = 1.0f;
+		vrManager.onAnalog(name, value, 0);
+		verify(vrManager).rotateCamera(value);
 	}
-
+	
+	/**
+	 * Test OnAnalog with fly camera Right.
+	 */
+	@Test
+	public void testOnAnalogFlyCameRight() {
+		String name = CameraInput.FLYCAM_RIGHT;
+		Float value = 1.0f;
+		vrManager.onAnalog(name, value, 0);
+		verify(vrManager).rotateCamera(-value);
+	}
 }
