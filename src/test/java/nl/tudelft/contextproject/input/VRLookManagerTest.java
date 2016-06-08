95a948b7101af7a1a6fb84436a0b082d9f464477
Diocruel <apocalytica8@hotmail.com>
2016-06-08 12:24:35 +0200

    Removed unused tests

--- src/test/java/nl/tudelft/contextproject/input/VRLookManagerTest.java
+++ src/test/java/nl/tudelft/contextproject/input/VRLookManagerTest.java
@@ -1,27 +1,46 @@
 package nl.tudelft.contextproject.input;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.jme3.input.CameraInput;
+import com.jme3.input.InputManager;
+import com.jme3.input.Joystick;
+import com.jme3.renderer.Camera;
+
+import nl.tudelft.contextproject.TestBase;
 
 /**
  * Test class for VR Look Manager.
  */
-public class VRLookManagerTest {
+public class VRLookManagerTest extends TestBase {
 
-	@Test
-	public void testVRLookManager() {
-		fail("Not yet implemented");
+	private VRLookManager vrManager;
+	/**
+	 * Set up a VRLookMAnager.
+	 */
+	@Before
+	public void setupTest() {
+		Camera mockedCam = mock(Camera.class);
+		vrManager = spy(new VRLookManager(mockedCam));
 	}
-
+	
+	/**
+	 * Register with input test.
+	 */
 	@Test
 	public void testRegisterWithInput() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMapJoystick() {
-		fail("Not yet implemented");
+		doNothing().when(vrManager).mapJoystick(any());
+		InputManager mockedInputManager = mock(InputManager.class);
+		
+		when(mockedInputManager.getJoysticks()).thenReturn(new Joystick[] {null});
+		vrManager.registerWithInput(mockedInputManager);
+		
+		verify(mockedInputManager).addListener(vrManager,  CameraInput.FLYCAM_LEFT, CameraInput.FLYCAM_RIGHT);
+		verify(vrManager).mapJoystick(null);
 	}
 
 	@Test
