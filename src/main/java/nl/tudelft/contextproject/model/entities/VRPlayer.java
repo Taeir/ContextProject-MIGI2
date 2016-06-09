b6340b7d1f62db793c961ef3fde031b9d0b275b5
Wytze Elhorst <welhorst@hotmail.nl>
2016-06-09 09:56:37 +0200

    action radius up, jump down and bunny attack and range up

--- src/main/java/nl/tudelft/contextproject/model/entities/VRPlayer.java
+++ src/main/java/nl/tudelft/contextproject/model/entities/VRPlayer.java
@@ -32,7 +32,7 @@ public class VRPlayer extends MovingEntity implements PhysicsObject, TickProduce
 	//Terminal velocity of the player
 	public static final float FALL_SPEED = 15f;
 	//How fast the player accelerates while falling
-	public static final float PLAYER_GRAVITY = 13f;
+	public static final float PLAYER_GRAVITY = 18f;
 
 
 	//Physical collision model.
@@ -47,7 +47,7 @@ public class VRPlayer extends MovingEntity implements PhysicsObject, TickProduce
 	//Constants for exploration.
 
 	public static final float EXPLORATION_INTERVAL = 0.5f;
-	public static final int EXPLORATION_RADIUS = 5;
+	public static final int EXPLORATION_RADIUS = 8;
 	
 	//Health constants
 	public static final float PLAYER_MAX_HEALTH = 5f;
