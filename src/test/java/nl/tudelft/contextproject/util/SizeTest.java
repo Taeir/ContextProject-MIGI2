949f3de45c85006a57832a64802241d1991d2640
Taico Aerts <info@taico.nl>
2016-06-09 02:40:35 +0200

    Removed unused import

--- src/test/java/nl/tudelft/contextproject/util/SizeTest.java
+++ src/test/java/nl/tudelft/contextproject/util/SizeTest.java
@@ -2,7 +2,6 @@ package nl.tudelft.contextproject.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
