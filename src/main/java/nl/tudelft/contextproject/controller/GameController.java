ab814f025add6f13bb3115033d7b968c36e38fa8
Chiel <chielbruin@tele2.nl>
2016-06-13 14:17:32 +0200

    Added missing files

---
+++ src/main/java/nl/tudelft/contextproject/controller/GameController.java
@@ -0,0 +1,55 @@
+package nl.tudelft.contextproject.controller;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.math.ColorRGBA;
+
+import nl.tudelft.contextproject.model.level.Level;
+
+/**
+ * A controller for the main game.
+ */
+public class GameController extends GameThreadController {
+
+	/**
+	 * Constructor for the game controller.
+	 *
+	 * @param app
+	 * 		The Main instance of this game
+	 * @param level
+	 * 		The level for this game
+	 * @param timeLimit
+	 * 		the time limit for this game
+	 */
+	public GameController(Application app, Level level, float timeLimit) {
+		super(app, level, timeLimit);
+	}
+	
+	/**
+	 * Create main game with a level loaded from a file.
+	 *
+	 * @param app
+	 * 		the main app that this controller is attached to
+	 * @param folder
+	 * 		the folder where to load the level from
+	 * @param timeLimit
+	 * 		the time limit for this game
+	 * @param isMap
+	 * 		if the level is a map, otherwise the map is a single room file
+	 */
+	public GameController(Application app, String folder, float timeLimit, boolean isMap) {
+		super(app, folder, timeLimit, isMap);
+	}
+	
+	@Override
+	public void initialize(AppStateManager stateManager, Application app) {
+		super.initialize(stateManager, app);
+		getHUD().showPopupText("Go!", ColorRGBA.White, 4);
+	}
+
+	@Override
+	public GameState getGameState() {
+		return GameState.RUNNING;
+	}
+
+}
