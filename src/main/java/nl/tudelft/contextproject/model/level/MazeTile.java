04c46777cdaf725f88cb887de35240bb629db725
Taico Aerts <info@taico.nl>
2016-06-14 16:19:55 +0200

    Only apply normal map on walls

--- src/main/java/nl/tudelft/contextproject/model/level/MazeTile.java
+++ src/main/java/nl/tudelft/contextproject/model/level/MazeTile.java
@@ -76,16 +76,21 @@ public class MazeTile implements Drawable, PhysicsObject {
 
 		Box box = new Box(.5f, .5f + height, .5f);
 		this.spatial = new Geometry("Box", box);
-		Material material = new Material(Main.getInstance().getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
 		TangentBinormalGenerator.generate(box);
+		
+		Material material = new Material(Main.getInstance().getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
 		material.setBoolean("UseMaterialColors", true);    
 		material.setColor("Diffuse", color);
 		material.setColor("Specular", ColorRGBA.White);
 		material.setFloat("Shininess", 64f);
 		material.setColor("Ambient", color);
 		material.setTexture("DiffuseMap", texture);
-		material.setTexture("NormalMap", Main.getInstance().getAssetManager().loadTexture("Textures/wallnormalmap.png"));
+		
+		if (this.type == TileType.WALL) {
+			material.setTexture("NormalMap", Main.getInstance().getAssetManager().loadTexture("Textures/wallnormalmap.png"));
+		}
 		material.setBoolean("UseMaterialColors", true);
+		
 		this.spatial.setMaterial(material);
 		this.spatial.move(position.x, height, position.y);
 		return spatial;
